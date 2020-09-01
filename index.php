378609b2bc158e014b863ab19508b54d23fb1039
GabrielePese <64837874+GabrielePese@users.noreply.github.com>
2020-09-01 17:01:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<h1> Ciao </h1>
+
+
+<?php
+
+$testo ='ciao prova';
+echo $testo;
+
+echo '<br>';
+
+
+$lunghezza = strlen($testo);
+echo $lunghezza;
+
+echo '<br>';
+
+$prova = $_GET["badword"];
+
+$sost = str_replace("$prova", "***", $testo);
+echo $sost;
+
+
+?>
